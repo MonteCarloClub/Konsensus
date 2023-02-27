6e64ff0e8fc0f413e06197522a1098ead9977f62
赵云潇 <zhao.yunxiao@outlook.com>
2023-02-27 22:54:37 +0800

    .

--- network/server.go
+++ network/server.go
@@ -2,10 +2,9 @@ package network
 
 import (
 	"encoding/json"
-	"fmt"
 
 	"github.com/MonteCarloClub/kchain-middleware/handler"
-	"github.com/Shopify/sarama"
+	"github.com/MonteCarloClub/log"
 	"github.com/yunxiaozhao/Konsensus/etcd"
 	"github.com/yunxiaozhao/Konsensus/kafka"
 )
@@ -21,31 +20,27 @@ func (s *Server) StartServer() {
 	s.Producer.InitProducer()
 	s.Putter.InitEtcdClient()
 	go s.Consumer.ReceiveFromKafka()
-	var msg *sarama.ConsumerMessage
-	for {
-		select {
-		case msg = <-s.Consumer.MessageChan:
-			var depositoryValue handler.DepositoryValue
-			fmt.Print("msg.Value: ", string(msg.Value), "\n")
-			if len(string(msg.Value)) < 2 || string(msg.Value)[0:2] != "0x" {
-				continue
-			}
-			data := s.Putter.GetFromEtcdKv(string(msg.Value))
-			fmt.Printf("#####%v\n", data)
-			if err := json.Unmarshal([]byte(data), &depositoryValue); err == nil {
-				depositoryValue.Status = "1"
-				depositoryValueJson, _ := json.Marshal(depositoryValue)
-				s.Putter.PutToEtcdKv(string(msg.Value), string(depositoryValueJson))
-			} else {
-				fmt.Println(err)
-			}
-
-			s.Producer.SendToKafka(string(msg.Value))
+	for msg := range s.Consumer.MessageChan {
+		var depositoryValue handler.DepositoryValue
+		log.Info("receive message from kafka", "value", string(msg.Value))
+		if len(string(msg.Value)) < 2 || string(msg.Value)[0:2] != "0x" {
+			continue
 		}
+		data := s.Putter.GetFromEtcdKv(string(msg.Value))
+		if err := json.Unmarshal([]byte(data), &depositoryValue); err == nil {
+			depositoryValue.Status = "1"
+			depositoryValueJson, _ := json.Marshal(depositoryValue)
+			s.Putter.PutToEtcdKv(string(msg.Value), string(depositoryValueJson))
+		} else {
+			log.Error("fail to unmarshal depository value", "err", err)
+		}
+
+		s.Producer.SendToKafka(string(msg.Value))
 	}
 }
 
 func (s *Server) StopServer() {
 	s.Consumer.KafkaConsumer.Close()
+	close(s.Consumer.MessageChan)
 	s.Producer.KafkaProducer.Close()
 }
