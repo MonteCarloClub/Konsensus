3243de179c11590fb848eca339fa544f0f097002
赵云潇 <zhao.yunxiao@outlook.com>
2023-03-22 16:43:14 +0800

    fix

---
+++ network/server_testing.go
@@ -0,0 +1,14 @@
+package network
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPBFT(t testing.B) {
+	for i:= 10001; i <= 11000; i++ {
+		fmt.Print("===============preprepare phase begins==================")
+		fmt.Print("Preprepare pack received")
+		fmt.Print("node")
+	}
+}
